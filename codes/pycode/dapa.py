ea5761fe8a665d871244eebb845dfac35bf3981c
jhjhc1483 <jfchae1483@naver.com>
2026-03-20 12:00:42 +0900

    Update dapa.py

--- codes/pycode/dapa.py
+++ codes/pycode/dapa.py
@@ -4,16 +4,27 @@ import pandas as pd
 import os
 import json
 
-# 환경 변수에서 API 키 불러오기
-SCRAPER_API_KEY = os.environ.get('SCRAPER_API_KEY')
-if not SCRAPER_API_KEY:
-    raise ValueError("GitHub Secrets에 SCRAPER_API_KEY가 설정되지 않았습니다.")
+import itertools
+
+# 환경 변수에서 API 키 불러오기 (로테이션 지원)
+API_KEYS = [
+    os.environ.get('SCRAPER_API_KEY'),
+    os.environ.get('SCRAPER_API_KEY_2')
+]
+# None이 아닌 키만 필터링
+SCRAPER_KEYS = [k for k in API_KEYS if k]
+
+if not SCRAPER_KEYS:
+    raise ValueError("GitHub Secrets 또는 .env에 SCRAPER_API_KEY가 설정되지 않았습니다.")
+
+# API 키 순환을 위한 이터레이터 생성
+key_cycle = itertools.cycle(SCRAPER_KEYS)
 
 SCRAPER_URL = 'http://api.scraperapi.com'
 target_url = "https://www.dapa.go.kr/dapa/doc/selectDocList.do?menuSeq=3069&bbsSeq=326"
 
 payload = {
-    'api_key': SCRAPER_API_KEY,
+    'api_key': next(key_cycle),
     'url': target_url,
     'country_code': 'kr' # 한국 IP 지정
 }
